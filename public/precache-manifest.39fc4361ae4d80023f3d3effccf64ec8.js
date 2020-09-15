dce7eda017a783a6a489ac6f3411a2408cb6e167
Enrico Gianoglio <enry8giano@gmail.com>
2020-09-15 20:53:34 +0200

    Full Project

---
+++ public/precache-manifest.39fc4361ae4d80023f3d3effccf64ec8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b53151c643e9cc2e3ab51e523e29d099",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5be4c2c4c21944b53ce",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "fd8b5ac8b8fa278f347f",
+    "url": "/static/js/2.512337b9.chunk.js"
+  },
+  {
+    "revision": "786089ef6a78275eeee7a67aba60641a",
+    "url": "/static/js/2.512337b9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c5be4c2c4c21944b53ce",
+    "url": "/static/js/main.78df18c9.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
